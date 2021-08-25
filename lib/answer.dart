4c8fa1a1879e86f6ffd4ff7d545224f234cd0d92
Adam Park <parksm2@uci.edu>
2021-08-25 07:59:52 -0700

    widgets

---
+++ lib/answer.dart
@@ -0,0 +1,19 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+import './main.dart';
+
+class Answer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: null,
+        child: Text('Answer 1'),
+      ),
+    );
+  }
+}
